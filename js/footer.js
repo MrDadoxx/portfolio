536dd0c930ed7912b2a60ecd9d10c071ae4f1379
Ezequias Torczuk <144610780+MrDadoxx@users.noreply.github.com>
2024-01-02 18:00:28 -0300

    Updated

--- js/footer.js
+++ js/footer.js
@@ -2,12 +2,9 @@ const notification = $("#notification");
 
 function copyUrl() {
   navigator.clipboard.writeText("+54 9 11 3668-9258");
-  notification.css("animation", "notification-up .2s ease-out 0s 1 forwards");
+  notification.style.animation = "notification-up .2s ease-out 0s 1 forwards"
 
-  setTimeout(() => {
-    notification.css(
-      "animation",
-      "notification-down .3s ease-in 0s 1 forwards"
-    );
-  }, 2000);
+setTimeout( () => {
+  notification.style.animation = "notification-down .3s ease-in 0s 1 forwards"
+}, 2000)
 }
